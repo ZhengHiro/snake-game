dd09e46160e6ce10e37284a651e5d618cd4df8eb
zjs <809702756@qq.com>
2020-10-26 00:07:54 +0800

    tmp commit

--- Snake.js
+++ Snake.js
@@ -4,7 +4,24 @@ const directY = [ 0, 1, 0, -1 ];
 export default class Snake {
   body = []; // [{x: 0, y: 0}]
 
-  move() {
+  constructor({ x, y }) {
+    this.body.push({ x, y });
+  }
 
+  assumeMove(direct) { // 假设朝某个方向移动
+
+  }
+
+  move(direct) { // 朝某个方向移动
+    let header = this.body[0];
+    let tail = this.body[this.body.length];
+    let newHeader = {
+      x: header.x + directX[direct],
+      y: header.y + directY[direct]
+    };
+    this.body.unshift(newHeader);
+    this.body.pop();
+
+    return this.body;
   }
 };
---
+++ World.js
@@ -0,0 +1,10 @@
+export default class Map {
+  body = []; // [{x: 0, y: 0}]
+
+  move(direct) { // 朝某个方向移动
+    this.body.unshift({
+
+    })
+  }
+
+};
